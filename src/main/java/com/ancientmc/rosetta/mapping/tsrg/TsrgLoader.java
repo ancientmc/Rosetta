9b5a0db7f994bd3a146ef1e4f88fe57e87de158a
moist-mason <67809772+moist-mason@users.noreply.github.com>
2026-02-13 10:16:35 -0600

    add loading class to create a TSRG object from a file. Uses SRGUtils. Works but need to add a sorter to ensure that the lines are in the proper natural order based on the input TSRG file.

---
+++ src/main/java/com/ancientmc/rosetta/mapping/tsrg/TsrgLoader.java
@@ -0,0 +1,97 @@
+package com.ancientmc.rosetta.mapping.tsrg;
+
+import com.ancientmc.rosetta.mapping.tsrg.type.*;
+import net.neoforged.srgutils.IMappingFile;
+import net.neoforged.srgutils.IMappingFile.IClass;
+import net.neoforged.srgutils.IMappingFile.IField;
+import net.neoforged.srgutils.IMappingFile.IMethod;
+import net.neoforged.srgutils.IMappingFile.IParameter;
+import net.neoforged.srgutils.INamedMappingFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Uses SRGUtils to create a TSRG object from a loaded file.
+ */
+public class TsrgLoader {
+    private final File file;
+
+    public TsrgLoader(File file) {
+        this.file = file;
+    }
+
+    public Tsrg load() throws IOException {
+        INamedMappingFile names = INamedMappingFile.load(file);
+        IMappingFile cnfMap = names.getMap("obf", "cnf");
+        IMappingFile idMap = names.getMap("obf", "id");
+
+        List<Tsrg.Line<? extends TsrgType>> lines = new LinkedList<>();
+        List<TsrgClass> tsrgClasses = new LinkedList<>();
+
+        for (IClass cnfClass : cnfMap.getClasses()) {
+            buildClass(cnfClass, idMap, lines, tsrgClasses);
+        }
+
+        return new Tsrg(file, lines, tsrgClasses);
+    }
+
+    public void buildClass(IMappingFile.IClass cnfClass, IMappingFile idMap, List<Tsrg.Line<? extends TsrgType>> lines, List<TsrgClass> tsrgClasses) {
+        List<TsrgField> childFields = new LinkedList<>();
+        List<TsrgMethod> childMethods = new LinkedList<>();
+
+        IMappingFile.IClass idClass = idMap.getClass(cnfClass.getOriginal());
+        String id = idClass.getMapped();
+        TsrgClass cls = new TsrgClass(cnfClass.getOriginal(), cnfClass.getMapped(), id);
+        lines.add(new Tsrg.Line<>(cls));
+
+        for (IField cnfField : cnfClass.getFields()) {
+            TsrgField field = getField(idClass, cnfField, cls);
+            lines.add(new Tsrg.Line<>(field));
+            childFields.add(field);
+        }
+
+        for (IMethod cnfMethod : cnfClass.getMethods()) {
+            TsrgMethod method = getMethod(idClass, cnfMethod, cls);
+            lines.add(new Tsrg.Line<>(method));
+            childMethods.add(method);
+
+            for (IParameter cnfParam : cnfMethod.getParameters()) {
+                TsrgParameter param = getParam(idClass, cnfParam, cnfMethod, method);
+                lines.add(new Tsrg.Line<>(param));
+            }
+        }
+
+        cls.setChildren(childFields, childMethods);
+        tsrgClasses.add(cls);
+    }
+
+    public TsrgField getField(IClass idClass, IField cnfField, TsrgClass parent) {
+        IField idField = idClass.getField(cnfField.getOriginal());
+        if (idField == null) return null;
+
+        String id = idField.getMapped();
+        return new TsrgField(cnfField.getOriginal(), cnfField.getMapped(), parent, id);
+    }
+
+    public TsrgMethod getMethod(IClass idClass, IMethod cnfMethod, TsrgClass parent) {
+        IMethod idMethod = idClass.getMethod(cnfMethod.getOriginal(), cnfMethod.getDescriptor());
+        if (idMethod == null) return null;
+
+        String id = idMethod.getMapped();
+        return new TsrgMethod(cnfMethod.getOriginal(), cnfMethod.getDescriptor(), cnfMethod.getMapped(), parent, id);
+    }
+
+    public TsrgParameter getParam(IClass idClass, IParameter cnfParam, IMethod cnfMethod, TsrgMethod parent) {
+        IMethod idMethod = idClass.getMethod(cnfMethod.getOriginal(), cnfMethod.getDescriptor());
+        if (idMethod == null) return null;
+
+        IParameter idParam = idMethod.getParameter(cnfParam.getIndex());
+        if (idParam == null) return null;
+
+        String id = idParam.getMapped();
+        return new TsrgParameter(idParam.getIndex(), idParam.getMapped(), parent, id);
+    }
+}
